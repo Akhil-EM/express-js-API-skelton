5447296b4f18e50a35d9b8f38801bdcea10ceedb
=Akhil-EM <=akhilemcalicut@gmail.com>
2022-07-11 13:10:21 +0530

    added run.bat file

---
+++ run.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+set "path=%cd%"
+cd  %path%/node_modules/express-api-skelton/
+
+ren ".env-dem" ".env"
+
+move ".env" "%path%"
+move "app.js" "%path%"
+move "public" "%path%"
+move "src" "%path%"
+
+rmdir /s /q "%path%\node_modules\express-api-skelton\" /s /q
+
+
+
+EXIT
