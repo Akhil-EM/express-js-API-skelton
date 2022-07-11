1758576019f890cdf3ca9bd8195bdc96971b7cbe
=Akhil-EM <=akhilemcalicut@gmail.com>
2022-07-11 13:11:46 +0530

    edited run.bat

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ move ".env" "%path%"
 move "app.js" "%path%"
 move "public" "%path%"
 move "src" "%path%"
-
+move ".gitignore" "%path%"
 rmdir /s /q "%path%\node_modules\express-api-skelton\" /s /q
 
 
